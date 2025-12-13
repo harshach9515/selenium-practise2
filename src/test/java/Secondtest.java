0cf9f5b1ddf61ffd9076f1f6a0132511efbc5269
sumanthreddy9676 <221288586+sumanthreddy9676@users.noreply.github.com>
2025-12-13 12:09:39 +0530

    Added Secondtest Files

--- src/test/java/Secondtest.java
+++ src/test/java/Secondtest.java
@@ -23,15 +23,24 @@ public class Secondtest {
         password.sendKeys("learning");
         signInBtn.click();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //print all title of the products
         List<WebElement> products =driver.findElements(By.cssSelector("div.card-body h4.card-title a"));
 
         for (WebElement product:products) {
             System.out.println(product.getText());
         }
-
         //Print all price of the products
+        List<WebElement> prices=driver.findElements(By.cssSelector("div.card-body h5"));
+        for (WebElement price:prices){
+            System.out.println(price.getText());
 
+        }
         //Print All product description
+        List<WebElement> texts=driver.findElements(By.cssSelector("div.card-body p.card-text"));
+        for (WebElement text:texts){
+            System.out.println(text.getText());
+        }
 
         System.out.println("Closing Browser...");
         driver.quit();
